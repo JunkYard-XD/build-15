DerpLab/platform_build

Clean up ROM flash and add DerpFest info
* Drop the target build fingerprint text.
* Drop the patching system image unconditionally text.
* Drop the verifying system image text.
* Changed the verified system image text.
* Add flashing system text.

Modified from original commit for DerpFest.

Change-Id: Icc2197b000dbaa70187a8e247a9faf28dbf59c4d
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

tools/releasetools/common.py
@@ -1991,9 +1991,8 @@ class BlockDifference(object):
                   write_verify_script=False):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
-    else:
-      script.Print("Patching %s image after verification." % (self.partition,))
+      script.Print(" ")
+      script.Print("Flashing DerpFest system partition...")
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -2093,7 +2092,6 @@ class BlockDifference(object):
 
   def WritePostInstallVerifyScript(self, script):
     partition = self.partition
-    script.Print('Verifying the updated %s image...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -2110,18 +2108,16 @@ class BlockDifference(object):
           'if range_sha1(%s, "%s") == "%s" then' % (
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
-      if partition == "system":
-        code = ErrorCode.SYSTEM_NONZERO_CONTENTS
-      else:
-        code = ErrorCode.VENDOR_NONZERO_CONTENTS
+      script.Print(" ")
+      script.Print("Verified DerpFest system partition...")
       script.AppendExtra(
           'else\n'
           '  abort("E%d: %s partition has unexpected non-zero contents after '
           'OTA update");\n'
           'endif;' % (code, partition))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified DerpFest system partition...")
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS

JunkYard-XD/build-15

tools/releasetools/common.py
@@ -3470,9 +3470,8 @@ class BlockDifference(object):
                   write_verify_script=False):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
-    else:
-      script.Print("Patching %s image after verification." % (self.partition,))
+      script.Print(" ")
+      script.Print("Flashing DerpFest System files...")
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -3572,7 +3571,6 @@ class BlockDifference(object):
 
   def WritePostInstallVerifyScript(self, script):
     partition = self.partition
-    script.Print('Verifying the updated %s image...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -3589,18 +3587,16 @@ class BlockDifference(object):
           'if range_sha1(%s, "%s") == "%s" then' % (
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
-      if partition == "system":
-        code = ErrorCode.SYSTEM_NONZERO_CONTENTS
-      else:
-        code = ErrorCode.VENDOR_NONZERO_CONTENTS
+      script.Print(" ")
+      script.Print("Verified DerpFest System files...")
       script.AppendExtra(
           'else\n'
           '  abort("E%d: %s partition has unexpected non-zero contents after '
           'OTA update");\n'
           'endif;' % (code, partition))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified DerpFest System files...")
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS